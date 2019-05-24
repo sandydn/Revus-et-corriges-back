b674a5401bc26bc70729b70e0cd114b9e6fcee68
Esoriak <somahaut.dev@gmail.com>
2019-05-25 00:32:07 +0200

    delete error index

--- conf.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '5b6ir8ps§',
-    database: 'calendar'
-});
-
-module.exports = connection;
