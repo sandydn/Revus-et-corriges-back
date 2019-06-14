953fff92712bfcf739613c1e7eb187b31244cda4
Nocktiss <anthony.grupposo@gmail.com>
2019-06-14 14:16:09 +0200

    Add environements variables + .env in gitignore

--- conf.js
+++ conf.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql');
+
+// Protect DB with .env //
+require('dotenv').config()
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '5b6ir8ps§',
-    database: 'calendar'
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 module.exports = connection;
